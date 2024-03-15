0535030bf06f0c02d2cd05e8922f8a5a226298f7
deninjose <deninjose0@gmail.com>
2024-03-15 13:29:09 +0530

    change

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -7,7 +7,7 @@ from .forms import PostForm, CommentForm
 from .models import Post, Comment
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
@@ -85,3 +85,4 @@ def comment_remove(request, pk):
 
 def approved_comments(self):
     return self.comments.filter(approved_comment=True)
+
